64c0094df839c972cdc8884cd84c8c28c34df5fb
MarinaBashechkina <128588136+MarinaBashechkina@users.noreply.github.com>
2023-04-19 11:11:49 +0300

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,12 @@
+document.getElementById('example').innerHTML="<p>Wow</p>";
+/*var child{
+	first{
+		name=mary;
+		age=10;
+	},
+	second{
+		name=garry;
+		age=8;
+	}
+}
+document.getElementById('ok')innerHTML="<>child.first.age</p>";
